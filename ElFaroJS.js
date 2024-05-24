94f55a128ebe308d9f9448d692f90ad7a3903cb7
Cotep8 <153141730+Cotep8@users.noreply.github.com>
2024-05-24 00:15:28 -0400

    Create ElFaroJS.js

---
+++ ElFaroJS.js
@@ -0,0 +1,8 @@
+let Contacto = [];
+
+function Mostrar() {
+    document.getElementById('Contacto').style.display ='block';
+}
+function Ocultar() {
+    document.getElementById('Contacto').style.display ='none';
+}
